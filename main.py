a35ad5f765783a037ca78ed41ea029b1990f76f6
cj5yd <cj5yd@mail.ru>
2019-03-25 21:17:47 +0600

    Changed the method of drawing of the field, added food, poison and walls, deleted some unused methods of bots, changed system of positioning of bots

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 
+
 food_health = 30
 wound_health = 20
 cell_size = 10
-length = 30
-height = 20
+length = 15
+height = 10
 win_length = length * cell_size + 200
 win_height = height * cell_size + 200
 
@@ -15,40 +16,22 @@ class Bot:
         """initiating of bot"""
         self.number = len(bots)+1
         self.health = 100
-        self.dna = 'abcbcba'
         self.pos = npos
         self.counter = 0
-        fieldmap[self.pos[1]][self.pos[0]] = self.number
-
-    def eat(self):
-        self.health += food_health
+        fieldmap[self.pos[0]][self.pos[1]] = self.number
 
     def move(self):
         new_pos = [self.pos[0]+1, self.pos[1]+1]
-        fieldmap[self.pos[1]][self.pos[0]] = 0
+        if fieldmap[new_pos[0]][new_pos[1]] != 0:
+            print('The place is not empty')
+            return
+        fieldmap[self.pos[0]][self.pos[1]] = 0
         self.pos = new_pos
-        fieldmap[self.pos[1]][self.pos[0]] = self.number
+        fieldmap[self.pos[0]][self.pos[1]] = self.number
 
     def info(self):
-        print('Bot №' + str(self.number) + ', health = ' + str(self.health) + '\n' + 'DNA: ' + self.dna + '\n'
-              + 'Counter = ' + str(self.counter) + '\n' + 'Position = ' + str(self.pos) + '\n')
-
-    def wound(self):
-        self.health -= wound_health
-
-    def act(self):
-        print('---------------------------------------------------------------')
-        self.info()
-        if self.dna[self.counter] == 'a':
-            self.eat()
-        if self.dna[self.counter] == 'b':
-            self.move()
-        if self.dna[self.counter] == 'c':
-            self.wound()
-
-        self.info()
-        print('---------------------------------------------------------------')
-        self.counter += 1
+        print('Bot №' + str(self.number) + ', health = ' + str(self.health) + '\n'  + 'Counter = ' + str(self.counter)
+              + '\n' + 'Position = ' + str(self.pos) + '\n')
 
 
 class Field:
@@ -60,28 +43,28 @@ class Field:
         pygame.display.set_caption('Evolution')
 
     def draw_field(self):
-        for i in range(self.length + 1):
-            x = (i + 1) * cell_size
-            pygame.draw.line(self.screen, (0, 0, 0), (x, cell_size), (x, (self.height+1) * cell_size))
-        for j in range(self.height + 1):
-            y = (j + 1) * cell_size
-            pygame.draw.line(self.screen, (0, 0, 0), (cell_size, y), ((self.length+1) * cell_size, y))
-        pygame.draw.rect(self.screen, (192, 192, 192), (0, 0, (self.length + 2) * cell_size+1, cell_size))
-        pygame.draw.rect(self.screen, (192, 192, 192),
-                         (0, (self.height+1)*cell_size+1, (self.length + 2) * cell_size+1, cell_size))
-        pygame.draw.rect(self.screen, (192, 192, 192), (0, cell_size, cell_size, self.height * cell_size+1))
-        pygame.draw.rect(self.screen, (192, 192, 192),
-                         ((self.length+1)*cell_size+1, cell_size, cell_size, self.height * cell_size+1))
+        for i in range(self.length + 5):
+            x = i * cell_size
+            pygame.draw.line(self.screen, (0, 0, 0), (x, 0), (x, (self.height+4) * cell_size))
+        for j in range(self.height + 5):
+            y = j * cell_size
+            pygame.draw.line(self.screen, (0, 0, 0), (0, y), ((self.length+4) * cell_size, y))
 
     def show_field(self):
         self.screen.fill((255, 255, 255))
         self.draw_field()
         for j in range(len(fieldmap)):
             for i in range(len(fieldmap[j])):
-                if fieldmap[j][i] >= 1:
-                    x = (i+1)*10+1
-                    y = (j+1)*10+1
+                x = i * 10 + 1
+                y = j * 10 + 1
+                if fieldmap[j][i] >= 1:         # bot
                     pygame.draw.rect(self.screen, (0, 0, 255), (x, y, 9, 9))
+                elif fieldmap[j][i] == -1:      # wall
+                    pygame.draw.rect(self.screen, (192, 192, 192), (x, y, 9, 9))
+                elif fieldmap[j][i] == -2:      # food
+                    pygame.draw.rect(self.screen, (0, 255, 0), (x, y, 9, 9))
+                elif fieldmap[j][i] == -3:      # poison
+                    pygame.draw.rect(self.screen, (255, 0, 0), (x, y, 9, 9))
         pygame.display.update()
 
 
@@ -92,30 +75,57 @@ clock.tick(60)
 Добавить возможность создания нескольких полей и связать каждого бота со своим полем
 Сделать чтобы изображение ботов накладывалось 
 
-
-
 '''
 
 pygame.init()
 
 # на карте цифры 1 и больше - порядковые номера ботов.
-# 0 - пустота, номера других объектов отрицательные: -1 - еда
-fieldmap = [[0 for i in range(length)] for j in range(height)]
+# 0 - пустота, номера других объектов отрицательные: -1 - стена, -2 - еда, -3 - яд
+
+
+def create_fieldmap():
+    fieldmap = [[0 for i in range(length+4)] for j in range(height+4)]
+    # делаем стены по краям поля
+    for i in range(length+4):
+        fieldmap[0][i] = -1
+        fieldmap[1][i] = -1
+        fieldmap[height + 2][i] = -1
+        fieldmap[height + 3][i] = -1
+    for j in range(height+4):
+        fieldmap[j][0] = -1
+        fieldmap[j][1] = -1
+        fieldmap[j][length + 2] = -1
+        fieldmap[j][length + 3] = -1
+    return fieldmap
+
+
+fieldmap = create_fieldmap()
 field = Field(length, height)
 field.draw_field()
-pos = [0, 0]
+pos = [5, 4]
 
 bots = []
 
 bots.append(Bot(pos))
-bots.append(Bot([1, 3]))
+bots.append(Bot([3, 4]))
 
 
-def field_click(pos):
-    fieldpos = [int(pos[0] / cell_size)-1, int(pos[1] / cell_size)-1]
-    if fieldmap[fieldpos[1]][fieldpos[0]] >= 1:
-        numb_of_bot = fieldmap[fieldpos[1]][fieldpos[0]]-1
-        bots[numb_of_bot].info()
+def field_click(click_pos):
+    fieldpos = [int(click_pos[1] / cell_size), int(click_pos[0] / cell_size)]
+    try:
+        if fieldmap[fieldpos[0]][fieldpos[1]] >= 1:
+            numb_of_bot = fieldmap[fieldpos[0]][fieldpos[1]]-1
+            bots[numb_of_bot].info()
+        if fieldmap[fieldpos[0]][fieldpos[1]] == 0:
+            print("There is empty")
+        if fieldmap[fieldpos[0]][fieldpos[1]] == -1:
+            print('This is a wall')
+        if fieldmap[fieldpos[0]][fieldpos[1]] == -2:
+            print('This is a food')
+        if fieldmap[fieldpos[0]][fieldpos[1]] == -3:
+            print('This is a poison')
+    except IndexError:
+        print("Out of the field")
 
 
 while True:
